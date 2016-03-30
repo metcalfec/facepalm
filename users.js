02946cd2f34bde1a1c1270dfbd6dfdd3e1503b43
Chris Metcalfe <metcalfe.c@gmail.com>
2016-03-30 09:27:40 -0700

    Initial database of users.

---
+++ users.js
@@ -0,0 +1,35 @@
+var users = [
+  {
+    id: 1,
+    name: 'Ted Bell',
+    about: [
+      'Owner of Ted\'s of Beverly Hills Steak House',
+      'Lives in Beverly Hills'
+    ],
+    friends: 2,
+    photos: 3,
+    image: 'images/placeholder_user.svg',
+    post: [
+      'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+    ]
+  },
+  {
+    id: 2,
+    name: 'Lloyd Bonafide',
+    about: [
+      'A Korean War Veteran',
+      'Lives in Alhambra, CA',
+      'Retired Heating & Plumbing Man'
+    ],
+    friends: 2,
+    photos: 3,
+    image: 'images/placeholder_user.svg',
+    post: [
+      'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+    ]
+  }
+];
+
+module.exports = users;
