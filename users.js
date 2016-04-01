cba119c06d1b979fd7b8617002da3fc126d464ed
Chris Metcalfe <metcalfe.c@gmail.com>
2016-04-01 09:40:56 -0700

    Updated users data.

--- users.js
+++ users.js
@@ -6,12 +6,31 @@ var users = [
       'Owner of Ted\'s of Beverly Hills Steak House',
       'Lives in Beverly Hills'
     ],
-    friends: 2,
+    friends: [
+      {
+        name: 'Art Griego',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Lloyd Bonafide',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Rick Sunderland',
+        image: 'images/placeholder_user.svg'
+      }
+    ],
+    friendsCount: 3,
     photos: 3,
     image: 'images/placeholder_user.svg',
+    banner: 'images/placeholder_jumbo.jpg',
     posts: [
       'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+      'Ex quem audire vix, veri dicant hendrerit et cum. Vix at percipitur dissentiet, ea utamur periculis est. Sumo aperiri his ut, his id falli mazim pericula. Omnium facilis ea per. No affert graecis his.',
+      'An mea fugit labitur fastidii, mel quot fugit eu. Eam cu appetere incorrupte, autem solet tamquam id duo, nam sonet persequeris suscipiantur an.',
+      'Fugit nonumes vel cu, vis omnes offendit ponderum id, ea sed laoreet deterruisset. Pro nobis solet volumus no, ea nemore prompta accusamus qui.',
+      'Pro in probo cotidieque, solet legimus ei duo. Fierent appellantur ullamcorper ad vis, ferri brute assum no vix.'
     ]
   },
   {
@@ -19,15 +38,139 @@ var users = [
     name: 'Lloyd Bonafide',
     about: [
       'A Korean War Veteran',
-      'Lives in Alhambra, CA',
-      'Retired Heating & Plumbing Man'
+      'Lives in Alhambra, CA'
     ],
-    friends: 2,
+    friends: [
+      {
+        name: 'Art Griego',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Jay Santos',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Ted Bell',
+        image: 'images/placeholder_user.svg'
+      }
+    ],
+    friendsCount: 3,
     photos: 3,
     image: 'images/placeholder_user.svg',
+    banner: 'images/placeholder_jumbo.jpg',
     posts: [
       'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
-      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+      'Ex quem audire vix, veri dicant hendrerit et cum. Vix at percipitur dissentiet, ea utamur periculis est. Sumo aperiri his ut, his id falli mazim pericula. Omnium facilis ea per. No affert graecis his.',
+      'An mea fugit labitur fastidii, mel quot fugit eu. Eam cu appetere incorrupte, autem solet tamquam id duo, nam sonet persequeris suscipiantur an.',
+      'Fugit nonumes vel cu, vis omnes offendit ponderum id, ea sed laoreet deterruisset. Pro nobis solet volumus no, ea nemore prompta accusamus qui.',
+      'Pro in probo cotidieque, solet legimus ei duo. Fierent appellantur ullamcorper ad vis, ferri brute assum no vix.'
+    ]
+  },
+  {
+    id: 3,
+    name: 'Rick Sunderland',
+    about: [
+      'Formerly Frodo the Puppet',
+      'Is made of fabric'
+    ],
+    friends: [
+      {
+        name: 'Ted Bell',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Jay Santos',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Art Griego',
+        image: 'images/placeholder_user.svg'
+      }
+    ],
+    friendsCount: 3,
+    photos: 3,
+    image: 'images/placeholder_user.svg',
+    banner: 'images/placeholder_jumbo.jpg',
+    posts: [
+      'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+      'Ex quem audire vix, veri dicant hendrerit et cum. Vix at percipitur dissentiet, ea utamur periculis est. Sumo aperiri his ut, his id falli mazim pericula. Omnium facilis ea per. No affert graecis his.',
+      'An mea fugit labitur fastidii, mel quot fugit eu. Eam cu appetere incorrupte, autem solet tamquam id duo, nam sonet persequeris suscipiantur an.',
+      'Fugit nonumes vel cu, vis omnes offendit ponderum id, ea sed laoreet deterruisset. Pro nobis solet volumus no, ea nemore prompta accusamus qui.',
+      'Pro in probo cotidieque, solet legimus ei duo. Fierent appellantur ullamcorper ad vis, ferri brute assum no vix.'
+    ]
+  },
+  {
+    id: 4,
+    name: 'Art Griego',
+    about: [
+      'Retired Pilot',
+      'Lives in Lancaster, CA'
+    ],
+    friends: [
+      {
+        name: 'Ted Bell',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Lloyd Bonafide',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Rick Sunderland',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Jay Santos',
+        image: 'images/placeholder_user.svg'
+      }
+    ],
+    friendsCount: 4,
+    photos: 3,
+    image: 'images/placeholder_user.svg',
+    banner: 'images/placeholder_jumbo.jpg',
+    posts: [
+      'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+      'Ex quem audire vix, veri dicant hendrerit et cum. Vix at percipitur dissentiet, ea utamur periculis est. Sumo aperiri his ut, his id falli mazim pericula. Omnium facilis ea per. No affert graecis his.',
+      'An mea fugit labitur fastidii, mel quot fugit eu. Eam cu appetere incorrupte, autem solet tamquam id duo, nam sonet persequeris suscipiantur an.',
+      'Fugit nonumes vel cu, vis omnes offendit ponderum id, ea sed laoreet deterruisset. Pro nobis solet volumus no, ea nemore prompta accusamus qui.',
+      'Pro in probo cotidieque, solet legimus ei duo. Fierent appellantur ullamcorper ad vis, ferri brute assum no vix.'
+    ]
+  },
+  {
+    id: 5,
+    name: 'Jay Santos',
+    about: [
+      'SubCommander of Citizens Auxilery Police',
+      'Location undisclosed'
+    ],
+    friends: [
+      {
+        name: 'Lloyd Bonafide',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Rick Sunderland',
+        image: 'images/placeholder_user.svg'
+      },
+      {
+        name: 'Art Griego',
+        image: 'images/placeholder_user.svg'
+      }
+    ],
+    friendsCount: 3,
+    photos: 3,
+    image: 'images/placeholder_user.svg',
+    banner: 'images/placeholder_jumbo.jpg',
+    posts: [
+      'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+      'Ex quem audire vix, veri dicant hendrerit et cum. Vix at percipitur dissentiet, ea utamur periculis est. Sumo aperiri his ut, his id falli mazim pericula. Omnium facilis ea per. No affert graecis his.',
+      'An mea fugit labitur fastidii, mel quot fugit eu. Eam cu appetere incorrupte, autem solet tamquam id duo, nam sonet persequeris suscipiantur an.',
+      'Fugit nonumes vel cu, vis omnes offendit ponderum id, ea sed laoreet deterruisset. Pro nobis solet volumus no, ea nemore prompta accusamus qui.',
+      'Pro in probo cotidieque, solet legimus ei duo. Fierent appellantur ullamcorper ad vis, ferri brute assum no vix.'
     ]
   }
 ];
